238fc13e23821f7ce3421fc67e5e05b469c75237
Bálint Áts <atsbalint@gmail.com>
2020-10-21 14:50:41 +0200

    init commit

---
+++ LeoIsland.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using UnityEngine;
+
+public class LeoIsland : MonoBehaviour
+{
+    public float epsilon;
+    public Vector2[] boundaries;  //the vertices of the island polygon
+
+    [SerializeField]
+    string fileToRead;
+    
+    public static int GetNumTrees(IEnumerable<Vector2> island, float epsilon)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Vector2[] LoadIsland(StreamReader sr)
+    {
+        List<Vector2> vectors = new List<Vector2>() ;
+        while (sr.Peek() >= 0)
+        {
+            string line = sr.ReadLine();
+            string[] coordinates = line.Split(',');
+            vectors.Add(new Vector2(float.Parse(coordinates[0], CultureInfo.InvariantCulture.NumberFormat),
+                                    float.Parse(coordinates[1], CultureInfo.InvariantCulture.NumberFormat)));
+
+        }
+        return vectors.ToArray();
+    }
+
+    void Awake()
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, fileToRead);
+        using (StreamReader sr = new StreamReader(path))
+        {
+            boundaries = LoadIsland(sr);
+        }
+        foreach(Vector2 vector in boundaries)
+        {
+            Debug.Log(vector.x);
+        }
+
+        CreateIslandMesh();
+    }
+
+    void CreateIslandMesh()
+    {
+        // Create Vector2 vertices
+        Vector2[] vertices2D = boundaries;
+
+        // Use the triangulator to get indices for creating triangles
+        Triangulator tr = new Triangulator(vertices2D);
+        int[] indices = tr.Triangulate();
+
+        // Create the Vector3 vertices
+        Vector3[] vertices = new Vector3[vertices2D.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, 0);
+        }
+
+        // Create the mesh
+        Mesh msh = new Mesh();
+        msh.vertices = vertices;
+        msh.triangles = indices;
+        msh.RecalculateNormals();
+        msh.RecalculateBounds();
+
+        // Set up game object with mesh;
+        gameObject.AddComponent(typeof(MeshRenderer));
+        MeshFilter filter = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+        filter.mesh = msh;
+    }
+}
---
+++ LeoTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class LeoTest : MonoBehaviour
+{
+    public TestCase[] testCases =
+    {
+         new TestCase { fileName = "Island1.txt", epsilon = 0.01f, expectedResult = 0 },
+         new TestCase { fileName = "Island2.txt", epsilon = 0.01f, expectedResult = 1 },
+         new TestCase { fileName = "Island3.txt", epsilon = 0.01f, expectedResult = 16 },
+         new TestCase { fileName = "Island4.txt", epsilon = 0.01f, expectedResult = 39 },
+         new TestCase { fileName = "Island1.txt", epsilon = 1.1f,  expectedResult = 0 },
+         new TestCase { fileName = "Island2.txt", epsilon = 1.1f,  expectedResult = 0 },
+         new TestCase { fileName = "Island3.txt", epsilon = 1.1f,  expectedResult = 4 },
+         new TestCase { fileName = "Island4.txt", epsilon = 1.1f,  expectedResult = 16 },
+    };
+
+    [ContextMenu("Run tests")]
+    public void RunTest()
+    {
+        foreach (var tc in testCases)
+        {
+            string path = Path.Combine(Application.streamingAssetsPath, tc.fileName);
+            //using (var stream = File.OpenRead(path))
+            using (StreamReader sr = new StreamReader(path))
+            {
+                var island = LeoIsland.LoadIsland(sr);
+                int result = LeoIsland.GetNumTrees(island, tc.epsilon);
+                Assert.AreEqual(result, tc.expectedResult, string.Format("Invalid result in test: {0}, E={1}", tc.fileName, tc.epsilon));
+            }
+        }
+    }
+
+    [Serializable]
+    public struct TestCase
+    {
+        public string fileName;
+        public float epsilon;
+        public int expectedResult;
+    }
+}
